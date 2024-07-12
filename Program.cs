438af706fdb09b63465f6c7b012a18886eaaf371
Pozzame <pozzame@gmail.com>
2024-07-12 11:46:20 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string[] lines = File.ReadAllLines("Partecipanti.txt");
+Random rnd = new Random();
+string scelto = lines[rnd.Next(lines.Length)];
+if (File.Exists("Scelti.txt") && File.ReadAllLines("Scelti.txt").Contains(scelto))
+    Console.WriteLine(scelto + " è già presente nella lista.");    
+else
+    File.AppendAllText("Scelti.txt", $"{scelto}\n");
